82936965a39d7b8f2bea3723aec935ac49353b8c
Simon Krajewski <simon@haxe.org>
2014-02-19 10:27:37 +0100

    add tests for unops and binops

--- test/Test.hx
+++ test/Test.hx
@@ -1,12 +1,69 @@
-import haxeparser.Data;
-import haxeparser.HaxeLexer;
-import haxeparser.HaxeParser;
-
 class Test extends haxe.unit.TestCase {
 	
 	static function main() {
 		var r = new haxe.unit.TestRunner();
 		r.add(new Test());
 		r.run();
+	}
+	
+	function testBinops() {
+		eeq("1 + 1");
+		eeq("1 - 1");
+		eeq("1 * 1");
+		eeq("1 / 1");
+		eeq("1 = 1");
+		eeq("1 == 1");
+		eeq("1 != 1");
+		eeq("1 > 1");
+		eeq("1 >= 1");
+		eeq("1 < 1");
+		eeq("1 <= 1");
+		eeq("1 & 1");
+		eeq("1 | 1");
+		eeq("1 ^ 1");
+		eeq("1 && 1");
+		eeq("1 || 1");
+		eeq("1 << 1");
+		eeq("1 >> 1");
+		eeq("1 >>> 1");
+		eeq("1 % 1");
+		eeq("1 ... 1");
+		eeq("1 => 1");
+	}
+	
+	function testAssignOps() {
+		eeq("1 += 1");
+		eeq("1 -= 1");
+		eeq("1 *= 1");
+		eeq("1 /= 1");
+		eeq("1 %= 1");
+		eeq("1 &= 1");
+		eeq("1 |= 1");
+		eeq("1 ^= 1");
+		eeq("1 <<= 1");
+		eeq("1 >>= 1");
+		eeq("1 >>>= 1");
+	}
+	
+	function testUnops() {
+		eeq("++1");
+		eeq("1++");
+		eeq("--1");
+		eeq("1--");
+		eeq("!1");
+		eeq("-a");
+		eeq("-1");
+		eeq("~1");
+	}
+	
+	static function parseExpr(inputCode:String, ?p:haxe.PosInfos) {
+		var parser = new haxeparser.HaxeParser(byte.ByteData.ofString(inputCode), '${p.methodName}:${p.lineNumber}');
+		var expr = parser.expr();
+		return haxe.macro.ExprTools.toString(expr);
+	}
+	
+	function eeq(inputCode:String, ?p:haxe.PosInfos) {
+		var inputParsed = parseExpr(inputCode, p);
+		assertEquals(inputCode, inputParsed, p);
 	}
 }
