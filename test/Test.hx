4a27812705d705801fef5b94728e86bedd23a384
Simon Krajewski <simon@haxe.org>
2014-02-19 10:47:25 +0100

    add more tests

--- test/Test.hx
+++ test/Test.hx
@@ -6,6 +6,37 @@ class Test extends haxe.unit.TestCase {
 		r.run();
 	}
 	
+	function testConst() {
+		eeq("1");
+		eeq("1.0");
+		eeq('"foo"');
+		eeq('foo');
+		eeq('~/.*/gimsu');
+	}
+	
+	function testHex() {
+		eeq("0xabcdef");
+		eeq("0xABCDEF");
+		eeq("0x0123456789");
+	}
+	
+	function testFloat() {
+		eeq(".1");
+		eeq("13e12");
+		eeq("13e-12");
+		eeq("13e+12");
+		eeq("13E12");
+		eeq("13E-12");
+		eeq("13E+12");
+		eeq("13.e12");
+		eeq("13.14e12");
+		eeq("13.14e-12");
+		eeq("13.14e+12");
+		eeq("13.14E12");
+		eeq("13.14E+12");
+		eeq("13.14E-12");
+	}
+	
 	function testBinops() {
 		eeq("1 + 1");
 		eeq("1 - 1");
@@ -54,6 +85,28 @@ class Test extends haxe.unit.TestCase {
 		eeq("-a");
 		eeq("-1");
 		eeq("~1");
+	}
+	
+	function testCall() {
+		eeq("a()");
+		eeq("a(b)");
+		eeq("a(b, c)");
+	}
+	
+	function testFieldAccess() {
+		eeq("a.b.c");
+		eeq("a.b().c()");
+	}
+	
+	function testArrayAccess() {
+		eeq("a[1]");
+		eeq("a[1][2]");
+		eeq("a[1][b[2]][3]");
+	}
+	
+	function testParenthesis() {
+		eeq("(1)");
+		eeq("((1))");
 	}
 	
 	static function parseExpr(inputCode:String, ?p:haxe.PosInfos) {
