3638656a2ed8a6ced88cc9d790906c7624d0a5a8
Simon Krajewski <simon@haxe.org>
2017-04-28 11:10:00 +0200

    avoid magic number

--- test/Test.hx
+++ test/Test.hx
@@ -411,28 +411,29 @@ class Test extends haxe.unit.TestCase {
 
 	function testIssue7(){
 		// stack overflow on large inputs
+		var k = 5000;
 		var s = "#if true 1 #else {";
-		for (i in 0 ... 5000) s+="1;";
+		for (i in 0 ... k) s+="1;";
 		s+= "} #end";
 		eeq(s, "1");
 
 		s = "#if true 1 #else ";
-		for (i in 0...5000) s += "#if true ";
+		for (i in 0...k) s += "#if true ";
 		s += "2";
-		for (i in 0...5000) s += " #end ";
+		for (i in 0...k) s += " #end ";
 		s += "#end";
 		eeq(s, "1");
 
 		s = "#if false ";
-		for (i in 0...5000) s += "#if true ";
+		for (i in 0...k) s += "#if true ";
 		s += "1";
-		for (i in 0...5000) s += " #end ";
+		for (i in 0...k) s += " #end ";
 		s += "#else 2";
 		s += "#end";
 		eeq(s, "2");
 
 		s = "#if false 1 ";
-		for (i in 0...5000) s += "#elseif false 1 ";
+		for (i in 0...k) s += "#elseif false 1 ";
 		s += "#else 2";
 		s += "#end";
 		eeq(s, "2");
